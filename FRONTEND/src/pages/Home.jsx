91482191555e75a6c0b4387516869ebc4d821c72
Shivam Yadav <shivam10yadav05@gmail.com>
2026-03-14 12:07:17 +0530

    Add hero section

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Title from '../components/Title'
+import HeroBanner from '../components/HeroBanner'
 
 const Home = () => {
   return (
     <div className='min-h-dvh flex flex-col items-center'>
+
+      <HeroBanner />
+
       <Title text1="All" text2="Collections" />
+
     </div>
   )
 }
