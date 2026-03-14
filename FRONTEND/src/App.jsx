0936366fe32d064340b5bed5a62a50b4d8873463
kuchtokrenge <kuchtokrenge2000@gmail.com>
2026-03-14 07:56:04 +0530

    Create frontend and installed tailwind + created assets.js to import and export all icons

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { assets } from './assets/assets'
+import Title from './components/Title'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
---
+++ FRONTEND/src/assets/assets.js
@@ -0,0 +1,18 @@
+import logo from "./icons/logo.png"
+import add_icon from "./icons/add_icon.png"
+import bin_icon from "./icons/bin_icon.png"
+import cart_icon from "./icons/cart_icon.png"
+import cross_icon from "./icons/cross_icon.png"
+import dropdown_icon from "./icons/dropdown_icon.png"
+import menu_icon from "./icons/menu_icon.png"
+import order_icon from "./icons/order_icon.png"
+import parcel_icon from "./icons/parcel_icon.svg"
+import profile_icon from "./icons/profile_icon.png"
+import search_icon from "./icons/search_icon.png"
+import star_dull_icon from "./icons/star_dull_icon.png"
+import star_icon from "./icons/star_icon.png"
+import upload_area from "./icons/upload_area.png"
+
+export const assets = {
+    logo,add_icon,bin_icon,cart_icon,cross_icon,dropdown_icon,menu_icon,order_icon,parcel_icon,profile_icon,search_icon,star_dull_icon,star_icon,upload_area
+}
