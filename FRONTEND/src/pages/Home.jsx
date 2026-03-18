0d6312b2ed94d8494f75362ed6dee79788ef7f6d
tuvarnitin <nitintuvar2003@gmail.com>
2026-03-19 00:20:11 +0530

    Create footer component

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -3,10 +3,11 @@ import Title from '../components/Title'
 import Hero from '../components/Hero'
 import ProductCard from '../components/ProductCard'
 import ProductPanel from '../components/ProductPanel'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='min-h-dvh w-full grid place-items-center'>
+    <div className='min-h-dvh w-full'>
       <Hero />
       <ProductPanel />
     </div>
