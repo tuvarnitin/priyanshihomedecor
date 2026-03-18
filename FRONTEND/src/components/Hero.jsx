0668eadb82168714974785dade84734c060ac59d
tuvarnitin <nitintuvar2003@gmail.com>
2026-03-19 00:14:41 +0530

    style: refine hero layout, spacing, and typography

--- FRONTEND/src/components/Hero.jsx
+++ FRONTEND/src/components/Hero.jsx
@@ -2,34 +2,31 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="w-full md:max-w-5xl sm:max-w-xl mx-auto px-4 py-10 md:px-20 pt-20">
+    <div className="w-full flex items-center md:max-w-6xl sm:max-w-xl mx-auto px-4 py-10 md:px-20 mt-6 md:mt-10">
       {/* Container using your border logic and background from @theme */}
-      <div className="w-full flex flex-col md:flex-row border border-foreground bg-background overflow-hidden md:h-95">
+      <div className="w-fit md:w-full mx-auto flex flex-col md:flex-row border border-foreground bg-background overflow-hidden md:h-110">
         
         {/* Left Content Side - Flex-grow to make it wider */}
-        <div className="w-full md:w-1/2 space-y-3 flex flex-col justify-center items-start px-6 md:py-10 py-4 md:px-12">
-          <div className="">
+        <div className="w-full md:w-1/2 flex flex-col gap-3 justify-center items-start px-6 md:py-10 py-4 md:px-12">
+          <div className="flex flex-col gap-1">
             {/* Using your outfit-bold font from index.css */}
-            <h2 className="text-responsive-medium leading-responsive-large text-foreground">
+            <h2 className="text-responsive-xlarge leading-responsive-xlarge text-foreground">
               Luxury Curtains
-            </h2>
-            <h2 className="text-responsive-medium leading-responsive-large text-foreground">
+              <br />
               For Modern Homes
             </h2>
-          </div>
-          
           {/* Using your outfit-light font from index.css */}
           <p className="text-responsive-small text-zinc-600">
             Elegant fabrics crafted for comfort and style.
           </p>
-
+          </div>
           <button className="px-[max(12px,1.8vw)] py-[max(6px,.6vw)] bg-foreground text-background text-responsive-small uppercase tracking-widest transition-opacity hover:opacity-90 cursor-pointer">
             Buy Now
           </button>
         </div>
 
         {/* Right Image Side - Separated by the vertical border line */}
-        <div className="md:w-1/2 w-full md:border-t-0 md:border-l overflow-hidden">
+        <div className="w-full h-full md:w-1/2 md:max-w-lg max-w-sm md:border-l border-t md:border-t-0 overflow-hidden">
           <img 
             src="/hero_image.png" 
             alt="Curtains"
